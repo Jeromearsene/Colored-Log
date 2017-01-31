15ad993bac0fb9ef9a988557977555e823fdcd99
Jerome <jeromearsene@gmail.com>
2017-01-31 10:27:56 +0100

    MAJ informations
    MAJ readme
    MAJ readme
    Instructions for rebuild
    MAJ readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Log
+#A simple Python 2.7 library for colored log
+
+For rebuild the package, use this command when you are in the folder: __python setup.py bdist_egg__
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
 setup(package = 'Log',
+	name = 'Python Colored Log',
+	homepage = 'https://github.com/Jeromearsene/Colored-log-Python-library',
     version = '1.0',
     author = "Jeromearsene",
     author_email = "jeromearsene@gmail.com",
